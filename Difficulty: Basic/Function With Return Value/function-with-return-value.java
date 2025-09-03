132414603277dffc8385750587f1706bac4e79e9
Udittya Basak <udittyabasak02@gmail.com>
2025-09-03 08:27:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With Return Value/function-with-return-value.java
@@ -0,0 +1,13 @@
+class Solution {
+    // code here
+    public static void main(String[] args){
+        int n = 2;
+        double ans = returnValueFunction(n);
+        System.out.println(ans);
+    }
+    
+    static int returnValueFunction(int n){
+        int a = n;
+        return a*2;
+    }
+}
